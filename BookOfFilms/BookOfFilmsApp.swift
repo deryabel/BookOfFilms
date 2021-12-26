09b0f796e082ef3cebf1157fc078860e12af6983
Derya Bel <deryaabel@hotmail.com>
2021-12-26 18:13:18 +0300

    Initial Commit

---
+++ BookOfFilms/BookOfFilmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookOfFilmsApp.swift
+//  BookOfFilms
+//
+//  Created by Derya Bel on 26.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct BookOfFilmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
